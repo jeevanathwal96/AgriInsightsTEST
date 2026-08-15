b097c8a3736f67a93179a0cbafd568c5508eb90d
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-08-15 12:19:05 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = '2026-08-15-280';
+var APP_VERSION = '2026-08-15-281';
 var CACHE = 'agriinsights-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
