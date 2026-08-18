39f673b25738eac1ba509def68af8cf3bf33fee6
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-08-18 16:55:27 +0100

    Orchard Fix v2 18AUG26

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = '2026-08-18-306';
+var APP_VERSION = '2026-08-18-307';
 var CACHE = 'agriinsights-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
