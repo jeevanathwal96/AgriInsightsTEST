60bf753e719eff79c6ec953c3fa15ea541e82fc5
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-08-18 11:30:51 +0100

    18AUG26 Spacing fix for Livestock

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = '2026-08-18-300';
+var APP_VERSION = '2026-08-18-301';
 var CACHE = 'agriinsights-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
