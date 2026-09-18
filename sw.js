9cb27ca96bac12f1142121e5687eb1e2518c067e
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-09-18 17:40:16 +0100

    Saving Sync

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = '2026-09-17-414';
+var APP_VERSION = '2026-09-17-415';
 var CACHE = 'agriinsights-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
