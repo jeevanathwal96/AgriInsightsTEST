ce22e70c87bac8dcfa17c0d82e534582a2a20358
jeevanathwal96 <jeevanathwal96@gmail.com>
2026-09-18 19:12:40 +0100

    Rainfall correction on logo

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
  */
 'use strict';
 
-var APP_VERSION = '2026-09-17-415';
+var APP_VERSION = '2026-09-18-416';
 var CACHE = 'agriinsights-' + APP_VERSION;
 
 /* App shell precached on install. The ?v=-suffixed JS is intentionally left to
